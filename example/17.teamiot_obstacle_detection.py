e57d91ff6e5492a9369d240fb3aa18e53c2dc4c2
Fuehnix <jacobfuehne@yahoo.com>
2025-02-14 20:10:33 -0600

    file naming updates

---
+++ example/17.teamiot_obstacle_detection.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Team IoT - CS437: Internet of Things
+# Lab 1 Part 1 Submission
+# Members: Mohammad Tamim, Jacob Fuehne, Jeffery Fan, KyoSook Shin
+# NetIDs: tamim2, jfuehne2, jfan16, kyosook2
